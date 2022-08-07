a6d7fc74f2b71b8e954689224255ef0558711b50
Arulmurugan-BCT <arulmurugan.s@bahwancybertek.com>
2022-08-07 17:50:12 +0530

    command akila repository code

--- src/repositories/akila.repository.ts
+++ src/repositories/akila.repository.ts
@@ -1,16 +1,16 @@
-import {inject} from '@loopback/core';
-import {DefaultCrudRepository} from '@loopback/repository';
-import {MysqlconnectionDataSource} from '../datasources';
-import {Akila, AkilaRelations} from '../models';
+// import {inject} from '@loopback/core';
+// import {DefaultCrudRepository} from '@loopback/repository';
+// import {MysqlconnectionDataSource} from '../datasources';
+// import {Akila, AkilaRelations} from '../models';
 
-export class AkilaRepository extends DefaultCrudRepository<
-  Akila,
-  typeof Akila.prototype.userName,
-  AkilaRelations
-> {
-  constructor(
-    @inject('datasources.mysqlconnection') dataSource: MysqlconnectionDataSource,
-  ) {
-    super(Akila, dataSource);
-  }
-}
+// export class AkilaRepository extends DefaultCrudRepository<
+//   Akila,
+//   typeof Akila.prototype.userName,
+//   AkilaRelations
+// > {
+//   constructor(
+//     @inject('datasources.mysqlconnection') dataSource: MysqlconnectionDataSource,
+//   ) {
+//     super(Akila, dataSource);
+//   }
+// }
